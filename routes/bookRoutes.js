3828886ff36f0e8bea080e4b2fef3840b8237101
Yeasir H <yeasir01@gmail.com>
2020-05-23 22:34:15 -0700

    required path module

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/booksController')
+const controller = require('../controllers/booksController');
+const path = require('path');
 
 router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
