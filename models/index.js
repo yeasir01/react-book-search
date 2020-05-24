d114d2e7461a727688e96ecbfec2376abbbb6e10
Yeasir H <yeasir01@gmail.com>
2020-05-23 22:35:19 -0700

    setup structure for models will work on it later

--- models/index.js
+++ models/index.js
@@ -2,15 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    title: String,
-    authors: [String],
-    description: String,
-    img: {
-        type: {String},
-    },
-    link: {
-        type: String,
-    }
+    
 });
 
 const Book = mongoose.model("Book", BookSchema);
