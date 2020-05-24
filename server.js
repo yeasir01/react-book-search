da0569a535d52097e705119680b1b2fdf19fcff2
Yeasir H <yeasir01@gmail.com>
2020-05-23 22:34:43 -0700

    completed express server setup

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Serve up static assets (usually on heroku)
+const PORT = process.env.PORT || 3001;
+
+require('dotenv').config()
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+var routes = require("./routes/bookRoutes.js");
+app.use(routes);
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> Server listening on port ${PORT}!`);
 });
