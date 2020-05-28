c3a9913161737ed6d7439dfd2d84b91a50386a1a
Yeasir H <yeasir01@gmail.com>
2020-05-28 03:25:26 -0700

    created database model

--- models/index.js
+++ models/index.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
-    
+    title: String,
+    description: String,
+    author: Array,
+    image: String,
+    link: String
 });
 
 const Book = mongoose.model("Book", BookSchema);
