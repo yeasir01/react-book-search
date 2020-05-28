0f16766c69f3478dc573bc15e7c6d655d0245996
Yeasir H <yeasir01@gmail.com>
2020-05-28 03:22:57 -0700

    setup body parser & mongo connections

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -9,9 +11,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
+  .then(()=> console.log('Mongoose has sucessfully connected!'))
+  .catch((err)=> console.log('There was an issue connecting to Mongo'));
+
 var routes = require("./routes/bookRoutes.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server listening on port ${PORT}!`);
+  console.log(`Server listening on port ${PORT}!`);
 });
