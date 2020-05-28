b0b073b05b8d198c0e17b1a4452f679f6f7f8dc9
Yeasir H <yeasir01@gmail.com>
2020-05-28 03:23:30 -0700

    refactor

--- client/src/error/index.js
+++ client/src/error/index.js
@@ -4,7 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Error = (props) =>{
     return(
         <div className="alert alert-danger text-center display-4" role="alert">
-            Somthing Went Wrong! <br/>{props.error}
+            Somthing Went Wrong!
+            <br/> 
+            {props.error}
         </div>
     )
 }
