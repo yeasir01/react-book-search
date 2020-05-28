9a2b7b8d466de9dc06ccb35d9082d6586aa50711
Yeasir H <yeasir01@gmail.com>
2020-05-28 03:25:04 -0700

    setup basic routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,7 +7,11 @@ router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-router.route("/api")
+router.route("/books/api")
     .get(controller.findBooks)
+    .post(controller.saveBook)
+
+router.route("/books/api/:id")
+    .delete(controller.removeBook)
 
 module.exports = router;
